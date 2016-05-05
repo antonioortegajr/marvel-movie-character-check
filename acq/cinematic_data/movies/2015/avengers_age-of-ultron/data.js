66ca1acfaffeb77768c34e06a7ccc8669a76c52d
Kronage <kronage@gmail.com>
2016-05-04 20:50:47 -0700

    Avengers Age of Ultron
    Added AoU cast to data

---
+++ acq/cinematic_data/movies/2015/avengers_age-of-ultron/data.js
@@ -0,0 +1,86 @@
+[{
+	"movies": [{
+		"title": "Avengers: Age of Ultron",
+		"release-year": 2015 ,
+		"production-company": "Marvel Studios",
+		"cast": [{
+			"": ""
+		}, {
+			"Iron Man": "Robert Downey Jr."
+		},
+    {
+      "Thor": "Chris Hemsworth"
+    },
+    {
+      "Hulk": "Mark Ruffalo"
+    },
+    {
+      "Captain America": "Chris Evens"
+    },
+    {
+      "Black Widow": "Scarlett Johannson"
+    },
+		{
+			"Hawkeye": "Jemery Renner"
+		},
+		{
+      "Ultron": "James Spader"
+    },
+    {
+      "Nick Fury": "Samuel L. Jackson"
+    },
+    {
+      "War Machine": "Don Cheadle"
+    },
+    {
+      "Quicksilver": "Aaron Taylor-Johnson"
+    },
+		{
+			"Scarlett Witch": "Elizabeth Olsen"
+		},
+		{
+      "Vision": "Paul Bettany"
+    },
+    {
+      "Maria Hill": "Cobie Smulders"
+    },
+    {
+      "Falcon": "Anthony Mackie"
+    },
+    {
+      "Peggy Carter": "Haley Atwell"
+    },
+		{
+			"Heimdell": "Idis Elba"
+		},
+		{
+      "Laura Barton": "Linda Cardellini"
+    },
+    {
+      "Erik Selvig": "Stellan Skarsgård"
+    },
+    {
+      "Dr. Helen Cho": "Claudia Kim"
+    },
+    {
+      "Strucker": "Thomas Kretschmann"
+    },
+		{
+			"Ulysses Klaue": "Andy Serkis"
+		},
+		{
+      "Madame B": "Julie Delpy" #The Russian during the Black Widow dream sequence
+    },
+    {
+      "Stan Lee": "Stan Lee"
+    },
+    {
+      "Dr. List": "	Henry Goodman"
+    },
+    {
+      "F.R.I.D.A.Y.": "Kerry Condon"
+    },]
+
+	}]
+
+}]
