5eb09fe767746166544195c26c392a75ba5f96c4
Kronage <kronage@gmail.com>
2016-05-09 22:32:49 -0700

    Marvel Phase Two

---
+++ acq/cinematic_data/movies/2014/captain-america_the-winter-soldier/data.js
@@ -0,0 +1,81 @@
+[{
+	"movies": [{
+		"title": "Captain America: The Winter Solider",#Movie Title
+		"release-year": 2014,
+		"production-company": "Marvel Studios",
+		"cast": #Character/Actor[{
+			"Captain America": "Chris Evans"
+		},
+		{
+			"Nick Fury": "Samuel L. Jackson"
+		},
+    {
+      "Black Widow": "Scarlett Johansson"
+    },
+    {
+      "Alexander Pierce": "Robert Redford"
+    },
+    {
+      "Winter Soldier": "Sebastian Stan"
+    },
+    {
+      "Falcon": "Anthony Mackie"
+    },
+		{
+			"Maria Hill": "Cobie Smulders"
+		},
+		{
+      "Brock Rumlow": "Frank Grillo"
+    },
+    {
+      "Jasper Sitwell": "Maximiliano Hernández"
+    },
+    {
+      "Agent 13": "Emily VanCamp"
+    },
+    {
+      "Peggy Carter": "Hayley Atwell"
+    },
+		{
+			"Dr. Arnim Zola": "Toby Jones"
+		},
+		{
+      "Smithsonian Guard": "Stan Lee"
+    },
+    {
+      "Jack Rollins": "Callan Mulvey"
+    },
+    {
+      "Councilwoman Hawley": "Jenny Agutter"
+    },
+    {
+      "Councilman Signh": "Bernard White"
+    },
+		{
+			"Councilman Rockwell": "Alan Dale"
+		},
+		{
+      "Councilman Yen": "Chin Han"
+    },
+    {
+      "Senator Stern": "Garry Shandling"
+    },
+    {
+      "Georges Batroc": "Georges St-Pierre"
+    },
+    {
+      "Dr. List": "Henry Goodman"
+    },
+		{
+			"Baron Wolfgang von Strucker": "Thomas Kretschmann"
+		},
+		{
+      "Wanda Maximoff": "Elizabeth Olsen"
+    },
+    {
+      "Pietro Maximoff": "Aaron Taylor-Johnson"
+    },]
+
+	}]
+
+}]
---
+++ acq/cinematic_data/movies/2015/ant-man/data.js
@@ -0,0 +1,72 @@
+[{
+	"movies": [{
+		"title": "Ant-Man",#Movie Title
+		"release-year": 2015,
+		"production-company": "Marvel Studios",
+		"cast": #Character/Actor[{
+			"Ant-Man": "Paul Rudd"
+		},
+		{
+			"Dr. Hank Pym": "Michael Douglas"
+		},
+    {
+      "Hope Van Dyne": "Evangeline Lily"
+    },
+    {
+      "Darren Cross": "Corey Stoll"
+    },
+    {
+      "Paxton": "Bobby Cannavale"
+    },
+    {
+      "Falcon": "Anthony Mackie"
+    },
+		{
+			"Maggie Lang": "Judy Greer"
+		},
+		{
+      "Cassie Lang": "Abby Ryder Fortson"
+    },
+    {
+      "Luis": "Michael Peña"
+    },
+    {
+      "Kurt": "David Dastmalchian"
+    },
+    {
+      "Dave": "T.I."
+    },
+		{
+			"Peggy Carter": "Hayley Atwell"
+		},
+		{
+      "Gale": "Wood Harris"
+    },
+    {
+      "Howard Stark": "John Slattery"
+    },
+    {
+      "Mitchell Carson": "Martin Donovan"
+    },
+    {
+      "Dale": "Gregg Turkington"
+    },
+		{
+			"Frank": "Joe Chrest"
+		},
+		{
+      "Janet Van Dyne": "Hayley Lovitt"
+    },
+    {
+      "Captain America": "Chris Evans"
+    },
+    {
+      "Bucky Barnes": "Sebastian Stan"
+    },
+    {
+      "Bartender": "Stan Lee"
+    },]
+
+	}]
+
+}]
