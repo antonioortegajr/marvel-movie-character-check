5628bcc3f3a17018fe79db8a6e7cf1d7b785a4cb
Kronage <kronage@gmail.com>
2016-05-10 19:44:43 -0700

    2013 Refactor

--- acq/cinematic_data/movies/2013/iron-man-3/data.js
+++ acq/cinematic_data/movies/2013/iron-man-3/data.js
@@ -1,86 +1,61 @@
 [{
 	"movies": [{
-		"title": "Iron Man 3",#Movie Title
+		"title": "Iron Man 3",
 		"release-year": 2013,
 		"production-company": "Marvel Studios",
-		"cast": #Character/Actor[{
+		"cast": [{
 			"Tony Stark": "Robert Downey Jr."
-		},
-		{
+		}, {
 			"Pepper Potts": "Gwyneth Paltrow"
-		},
-    {
-      "Colonel James Rhodes": "Done Cheadle"
-    },
-    {
-      "Aldrich Killian": "Guy Pearce"
-    },
-    {
-      "Maya Hansen": "Rebecca Hall"
-    },
-    {
-      "Happy Hogan": "Jon Favreau"
-    },
-		{
+		}, {
+			"Colonel James Rhodes": "Done Cheadle"
+		}, {
+			"Aldrich Killian": "Guy Pearce"
+		}, {
+			"Maya Hansen": "Rebecca Hall"
+		}, {
+			"Happy Hogan": "Jon Favreau"
+		}, {
 			"Trever Slattery": "Ben Kingsley"
-		},
-		{
-      "Savin": "James Badge Dale"
-    },
-    {
-      "Brandt": "Stephanie Szostak"
-    },
-    {
-      "Jarvis": "Paul Bettany"
-    },
-    {
-      "President Ellis": "William Sadler"
-    },
-		{
+		}, {
+			"Savin": "James Badge Dale"
+		}, {
+			"Brandt": "Stephanie Szostak"
+		}, {
+			"Jarvis": "Paul Bettany"
+		}, {
+			"President Ellis": "William Sadler"
+		}, {
 			"Mrs. Davis": "Dale Dickey"
-		},
-		{
-      "Harley Keener": "Ty Simpkins"
-    },
-    {
-      "Vice President Rodriguez": "Miguel Ferrer"
-    },
-    {
-      "Droctor Wu": "Xueqi Wang"
-    },
-    {
-      "Wu Jiaqi": "Bingbing Fan"
-    },
-		{
+		}, {
+			"Harley Keener": "Ty Simpkins"
+		}, {
+			"Vice President Rodriguez": "Miguel Ferrer"
+		}, {
+			"Droctor Wu": "Xueqi Wang"
+		}, {
+			"Wu Jiaqi": "Bingbing Fan"
+		}, {
 			"Ho Yinsen": "Shaun Toub"
-		},
-		{
-      "Pat Kiernan": "Pat Kiernan"
-    },
-    {
-      "Josh Elliott": "Josh Elliott"
-    },
-    {
-      "Megan Henderson": "Megan Henderson"
-    },
-    {
-      "Thomas Roberts": "Thomas Roberts"
-    },
-		{
+		}, {
+			"Pat Kiernan": "Pat Kiernan"
+		}, {
+			"Josh Elliott": "Josh Elliott"
+		}, {
+			"Megan Henderson": "Megan Henderson"
+		}, {
+			"Thomas Roberts": "Thomas Roberts"
+		}, {
 			"Bill Maher": "Bill Maher"
-		},
-		{
-      "Joan Rivers": "Joan Rivers"
-    },
-    {
-      "George Kotsiopoulos": "George Kotsiopoulos"
-    },
-    {
-      "Taggart": "Ashley Hamilton"
-    },
-    {
-      "Pageant Judge": "Stan Lee"
-    },]
+		}, {
+			"Joan Rivers": "Joan Rivers"
+		}, {
+			"George Kotsiopoulos": "George Kotsiopoulos"
+		}, {
+			"Taggart": "Ashley Hamilton"
+		}, {
+			"Pageant Judge": "Stan Lee"
+		}, ]
 
 	}]
 
--- acq/cinematic_data/movies/2013/thor_the-dark-world/data.js
+++ acq/cinematic_data/movies/2013/thor_the-dark-world/data.js
@@ -1,89 +1,63 @@
 [{
 	"movies": [{
-		"title": "Thor: The Dark World",#Movie Title
+		"title": "Thor: The Dark World",
 		"release-year": 2013,
 		"production-company": "Marvel Studios",
-		"cast": #Character/Actor[{
+		"cast": [{
 			"Thor": "Chris Hemsworth"
-		},
-		{
+		}, {
 			"Jane Foster": "Natalie Portman"
-		},
-    {
-      "Loki": "Tom Hiddleston"
-    },
-    {
-      "Odin": "Anthony Hopkins"
-    },
-    {
-      "Malekith": "Christopher Eccleston"
-    },
-    {
-      "Sif": "Jaimie Alexander"
-    },
-		{
+		}, {
+			"Loki": "Tom Hiddleston"
+		}, {
+			"Odin": "Anthony Hopkins"
+		}, {
+			"Malekith": "Christopher Eccleston"
+		}, {
+			"Sif": "Jaimie Alexander"
+		}, {
 			"Fandral": "Zachary Levi"
-		},
-		{
-      "Volstagg": "Ray Stevenson"
-    },
-    {
-      "Hogun": "Tadanobu Asano"
-    },
-    {
-      "Heimdall": "Idris Elba"
-    },
-    {
-      "Frigga": "Rene Russo"
-    },
-		{
+		}, {
+			"Volstagg": "Ray Stevenson"
+		}, {
+			"Hogun": "Tadanobu Asano"
+		}, {
+			"Heimdall": "Idris Elba"
+		}, {
+			"Frigga": "Rene Russo"
+		}, {
 			"Kurse": "Adewale Akinnuoye-Agbaje"
-		},
-		{
-      "Darcy Lewis": "Kat Dennings"
-    },
-    {
-      "Erik Selvig": "Stellan Skarsgård"
-    },
-    {
-      "Eir": "Alice Krige"
-    },
-    {
-      "Tyr": "Clive Russell"
-    },
-		{
+		}, {
+			"Darcy Lewis": "Kat Dennings"
+		}, {
+			"Erik Selvig": "Stellan Skarsgård"
+		}, {
+			"Eir": "Alice Krige"
+		}, {
+			"Tyr": "Clive Russell"
+		}, {
 			"Ian Boothby": "Jonathan Howard"
-		},
-		{
-      "John": "Romone Morgan"
-    },
-    {
-      "Navid": "Obada Alassadi"
-    },
-    {
-      "Maddie": "Imaan Chentouf"
-    },
-    {
-      "Volstagg's Wife": "Clair Brown"
-    },
-		{
+		}, {
+			"John": "Romone Morgan"
+		}, {
+			"Navid": "Obada Alassadi"
+		}, {
+			"Maddie": "Imaan Chentouf"
+		}, {
+			"Volstagg's Wife": "Clair Brown"
+		}, {
 			"Richard": "Chris O'Dowd"
-		},
-		{
-      "Stan Lee": "Stan Lee"
-    },
-    {
-      "Bor": "Tony Curran"
-    },
-    {
-      "The Collector": "Benicio Del Toro"
-    },
-    {
-      "Loki as Captain America": "Chris Evans"
-    },
-		{
+		}, {
+			"Stan Lee": "Stan Lee"
+		}, {
+			"Bor": "Tony Curran"
+		}, {
+			"The Collector": "Benicio Del Toro"
+		}, {
+			"Loki as Captain America": "Chris Evans"
+		}, {
 			"Carina": "Ophelia Lovibond"
-		},]
+		}, ]
 
 	}]
 
