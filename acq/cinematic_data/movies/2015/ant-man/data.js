3903d01c37277b45f86c9669026b0c2f8c1466bf
Kronage <kronage@gmail.com>
2016-05-10 19:48:44 -0700

    2015 Refactor

--- acq/cinematic_data/movies/2015/ant-man/data.js
+++ acq/cinematic_data/movies/2015/ant-man/data.js
@@ -1,71 +1,51 @@
 [{
 	"movies": [{
-		"title": "Ant-Man",#Movie Title
+		"title": "Ant-Man",
 		"release-year": 2015,
 		"production-company": "Marvel Studios",
-		"cast": #Character/Actor[{
+		"cast": [{
 			"Ant-Man": "Paul Rudd"
-		},
-		{
+		}, {
 			"Dr. Hank Pym": "Michael Douglas"
-		},
-    {
-      "Hope Van Dyne": "Evangeline Lily"
-    },
-    {
-      "Darren Cross": "Corey Stoll"
-    },
-    {
-      "Paxton": "Bobby Cannavale"
-    },
-    {
-      "Falcon": "Anthony Mackie"
-    },
-		{
+		}, {
+			"Hope Van Dyne": "Evangeline Lily"
+		}, {
+			"Darren Cross": "Corey Stoll"
+		}, {
+			"Paxton": "Bobby Cannavale"
+		}, {
+			"Falcon": "Anthony Mackie"
+		}, {
 			"Maggie Lang": "Judy Greer"
-		},
-		{
-      "Cassie Lang": "Abby Ryder Fortson"
-    },
-    {
-      "Luis": "Michael Peña"
-    },
-    {
-      "Kurt": "David Dastmalchian"
-    },
-    {
-      "Dave": "T.I."
-    },
-		{
+		}, {
+			"Cassie Lang": "Abby Ryder Fortson"
+		}, {
+			"Luis": "Michael Peña"
+		}, {
+			"Kurt": "David Dastmalchian"
+		}, {
+			"Dave": "T.I."
+		}, {
 			"Peggy Carter": "Hayley Atwell"
-		},
-		{
-      "Gale": "Wood Harris"
-    },
-    {
-      "Howard Stark": "John Slattery"
-    },
-    {
-      "Mitchell Carson": "Martin Donovan"
-    },
-    {
-      "Dale": "Gregg Turkington"
-    },
-		{
+		}, {
+			"Gale": "Wood Harris"
+		}, {
+			"Howard Stark": "John Slattery"
+		}, {
+			"Mitchell Carson": "Martin Donovan"
+		}, {
+			"Dale": "Gregg Turkington"
+		}, {
 			"Frank": "Joe Chrest"
-		},
-		{
-      "Janet Van Dyne": "Hayley Lovitt"
-    },
-    {
-      "Captain America": "Chris Evans"
-    },
-    {
-      "Bucky Barnes": "Sebastian Stan"
-    },
-    {
-      "Bartender": "Stan Lee"
-    },]
+		}, {
+			"Janet Van Dyne": "Hayley Lovitt"
+		}, {
+			"Captain America": "Chris Evans"
+		}, {
+			"Bucky Barnes": "Sebastian Stan"
+		}, {
+			"Bartender": "Stan Lee"
+		}, ]
 
 	}]
 
