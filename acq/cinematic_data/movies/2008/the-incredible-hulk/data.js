a08fd6ed1803bb4289a6148f2c23decb2bade39c
Kronage <kronage@gmail.com>
2016-05-10 19:35:59 -0700

    Hulk Refactor

--- acq/cinematic_data/movies/2008/the-incredible-hulk/data.js
+++ acq/cinematic_data/movies/2008/the-incredible-hulk/data.js
@@ -5,49 +5,35 @@
 		"production-company": "Marvel Studios",
 		"cast": [{
 			"Bruce Banner": "Edward Norton"
-		},
-    {
+		}, {
 			"Betty Ross": "Liv Tyler"
-		},
-    {
-      "Emil Blonsky": "Tim Roth"
-    },
-    {
-      "General 'Thunderbolt' Ross": "William Hurt"
-    },
-    {
-      "Samuel Sterns": "Tim Blake Nelson"
-    },
-    {
-      "Leonard": "Ty Burrell"
-    },
-		{
+		}, {
+			"Emil Blonsky": "Tim Roth"
+		}, {
+			"General 'Thunderbolt' Ross": "William Hurt"
+		}, {
+			"Samuel Sterns": "Tim Blake Nelson"
+		}, {
+			"Leonard": "Ty Burrell"
+		}, {
 			"Major Kathleen Sparr": "Christina Cabot"
-		},
-		{
-      "General Joe Greller": "Peter Mensah"
-    },
-    {
-      "The Incredible Hulk": "Lou Ferigno"
-    },
-    {
-      "Stanley": "Paul Soles"
-    },
-    {
-      "Martina": "Débora Nascimento"
-    },
-		{
+		}, {
+			"General Joe Greller": "Peter Mensah"
+		}, {
+			"The Incredible Hulk": "Lou Ferigno"
+		}, {
+			"Stanley": "Paul Soles"
+		}, {
+			"Martina": "Débora Nascimento"
+		}, {
 			"Computer Nerd": "Martin Starr"
-		},
-		{
-      "Harlem Bystander": "Michael Kennth Williams"
-    },
-    {
-      "Milwaukee Man Drinking From Bottle": "Stan Lee"
-    },
-    {
-      "Tony Stark": "Robert Downey Jr."
-    }]
+		}, {
+			"Harlem Bystander": "Michael Kennth Williams"
+		}, {
+			"Milwaukee Man Drinking From Bottle": "Stan Lee"
+		}, {
+			"Tony Stark": "Robert Downey Jr."
+		}]
 
 	}]
 
