5e017b2a9d8aca72d065c333a659131c35b6f6bf
Kronage <kronage@gmail.com>
2016-05-10 19:46:38 -0700

    2014 Refactor

--- acq/cinematic_data/movies/2014/guardians-of-the-galaxy/data.js
+++ acq/cinematic_data/movies/2014/guardians-of-the-galaxy/data.js
@@ -1,86 +1,61 @@
 [{
 	"movies": [{
-		"title": "Guardians of the Galaxy",#Movie Title
+		"title": "Guardians of the Galaxy",
 		"release-year": 2014,
 		"production-company": "Marvel Studios",
-		"cast": #Character/Actor[{
+		"cast": [{
 			"Peter Quill": "Chris Pratt"
-		},
-		{
+		}, {
 			"Gamora": "Zoe Saldana"
-		},
-    {
-      "Drax": "Dave Bautista"
-    },
-    {
-      "Groot": "Vin Diesel"
-    },
-    {
-      "Rocket": "Bradley Cooper"
-    },
-    {
-      "Ronan": "Lee Pace"
-    },
-		{
+		}, {
+			"Drax": "Dave Bautista"
+		}, {
+			"Groot": "Vin Diesel"
+		}, {
+			"Rocket": "Bradley Cooper"
+		}, {
+			"Ronan": "Lee Pace"
+		}, {
 			"Yondu Udonta": "Michael Rooker"
-		},
-		{
-      "Nebula": "Karen Gillan"
-    },
-    {
-      "Korath": "Djimon Hounsou"
-    },
-    {
-      "Corpsman Dey": "John C. Reiliy"
-    },
-    {
-      "Nova Prime": "Glenn Close"
-    },
-		{
+		}, {
+			"Nebula": "Karen Gillan"
+		}, {
+			"Korath": "Djimon Hounsou"
+		}, {
+			"Corpsman Dey": "John C. Reiliy"
+		}, {
+			"Nova Prime": "Glenn Close"
+		}, {
 			"The Collector": "Benicio Del Toro"
-		},
-		{
-      "Meredith Quill": "Laura Haddock"
-    },
-    {
-      "Kraglin": "Sean Gunn"
-    },
-    {
-      "Denarian Saal": "Peter Serafinowicz"
-    },
-    {
-      "The Broker": "Christopher Fairbank"
-    },
-		{
+		}, {
+			"Meredith Quill": "Laura Haddock"
+		}, {
+			"Kraglin": "Sean Gunn"
+		}, {
+			"Denarian Saal": "Peter Serafinowicz"
+		}, {
+			"The Broker": "Christopher Fairbank"
+		}, {
 			"Grandpa Quill": "Gregg Henry"
-		},
-		{
-      "Bereet": "Melia Kreiling"
-    },
-    {
-      "Horuz": "Tom Proctor"
-    },
-    {
-      "Xandarian Ladies' Man": "Stan Lee"
-    },
-    {
-      "The Other": "Alexis Denisof"
-    },
-		{
+		}, {
+			"Bereet": "Melia Kreiling"
+		}, {
+			"Horuz": "Tom Proctor"
+		}, {
+			"Xandarian Ladies' Man": "Stan Lee"
+		}, {
+			"The Other": "Alexis Denisof"
+		}, {
 			"Carina": "Ophelia Lovibond"
-		},
-		{
-      "Cosmo the Space Dog": "Fred"
-    },
-    {
-      "Thanos": "Josh Brolin"
-    },
-    {
-      "Howard the Duck": "Seth Green"
-    },
-    {
-      "Monstrous Inmate": "Nathan Fillion"
-    },]
+		}, {
+			"Cosmo the Space Dog": "Fred"
+		}, {
+			"Thanos": "Josh Brolin"
+		}, {
+			"Howard the Duck": "Seth Green"
+		}, {
+			"Monstrous Inmate": "Nathan Fillion"
+		}, ]
 
 	}]
 
