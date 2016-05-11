3b57dd45056c9269108dfbd0c479a667ee302e62
Kronage <kronage@gmail.com>
2016-05-10 19:37:17 -0700

    Iron Man 2 Refactor

--- acq/cinematic_data/movies/2010/iron-man-2/data.js
+++ acq/cinematic_data/movies/2010/iron-man-2/data.js
@@ -1,92 +1,65 @@
 [{
 	"movies": [{
-		"title": "Iron Man 2",#Movie Title
+		"title": "Iron Man 2",
 		"release-year": 2010,
 		"production-company": "Marvel Studios",
-		"cast": #Character/Actor[{
+		"cast": [{
 			"Tony Stark": "Robert Downey Jr."
-		},
-    {
+		}, {
 			"Pepper Potts": "Gwyneth Paltrow"
-		},
-    {
-      "Lt. Col. James 'Rhodey' Rhodes": "Don Cheadle"
-    },
-    {
-      "Natasha Romanoff": "Scarlett Johansson"
-    },
-    {
-      "Justin Hammer": "Sam Rockwell"
-    },
-    {
-      "Ivan Vanko": "Mickey Rourke"
-    },
-		{
+		}, {
+			"Lt. Col. James 'Rhodey' Rhodes": "Don Cheadle"
+		}, {
+			"Natasha Romanoff": "Scarlett Johansson"
+		}, {
+			"Justin Hammer": "Sam Rockwell"
+		}, {
+			"Ivan Vanko": "Mickey Rourke"
+		}, {
 			"Nick Fury": "Samuel L. Jackson"
-		},
-		{
-      "Agent Coulson": "Clark Gregg"
-    },
-    {
-      "Howard Stark": "John Slattery"
-    },
-    {
-      "Senator Stern": "Garry Shandling"
-    },
-    {
-      "Jarvis": "Paul Bettany"
-    },
-		{
+		}, {
+			"Agent Coulson": "Clark Gregg"
+		}, {
+			"Howard Stark": "John Slattery"
+		}, {
+			"Senator Stern": "Garry Shandling"
+		}, {
+			"Jarvis": "Paul Bettany"
+		}, {
 			"U.S. Marshal": "Kate Mara"
-		},
-		{
-      "Christine Everhart": "Leslie Bibb"
-    },
-    {
-      "Happy Hogan": "Jon Favreau"
-    },
-    {
-      "	Christiane Amanpour": "	Christiane Amanpour"
-    },
-    {
-      "Detective Lemieux": "Philppe Bergeron"
-    },
-		{
+		}, {
+			"Christine Everhart": "Leslie Bibb"
+		}, {
+			"Happy Hogan": "Jon Favreau"
+		}, {
+			"Christiane Amanpour": "Christiane Amanpour"
+		}, {
+			"Detective Lemieux": "Philppe Bergeron"
+		}, {
 			"Adam Goldtein": "DJ AM"
-		},
-		{
-      "Major Allen": "Tim Guinee"
-    },
-    {
-      "General Meade": "Eric L. Haney"
-    },
-    {
-      "Anton Vanko": "Evgeniy Lazarev"
-    },
-    {
-      "Stan Lee": "Stan Lee"
-    },
-		{
+		}, {
+			"Major Allen": "Tim Guinee"
+		}, {
+			"General Meade": "Eric L. Haney"
+		}, {
+			"Anton Vanko": "Evgeniy Lazarev"
+		}, {
+			"Stan Lee": "Stan Lee"
+		}, {
 			"Rebecca": "Helena Mattsson"
-		},
-		{
-      "Rebeka": "Anya Monzikova"
-    },
-    {
-      "Bambi Arbogast": "Margy Stein"
-    },
-    {
-      "Chess Roberts": "Olivia Munn"
-    },
-    {
-      "Elon Musk": "Elon Musk"
-    },
-		{
+		}, {
+			"Rebeka": "Anya Monzikova"
+		}, {
+			"Bambi Arbogast": "Margy Stein"
+		}, {
+			"Chess Roberts": "Olivia Munn"
+		}, {
+			"Elon Musk": "Elon Musk"
+		}, {
 			"Bill O'Reiliy": "Bill O'Reiliy"
-		},
-		{
-      "Jack": "Jack White"
-    }]
+		}, {
+			"Jack": "Jack White"
+		}]
 
 	}]
 
